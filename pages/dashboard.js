bd47730d551b0542b663f2042bb9255fd078195a
Nabil <nabilaltamash@gmail.com>
2022-07-18 22:16:14 +0600

    Changed page function names to avoid Vercel error

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const dashboard = () => {
-    return (
-        <div>
-            <h2>Welcome to my Dashboard</h2>
-        </div>
-    );
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Welcome to my Dashboard</h2>
+    </div>
+  );
 };
 
-export default dashboard;
+export default Dashboard;
